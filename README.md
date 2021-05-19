974ff44caa03e00e0015362e66b749bdf1dcbec7
Fabio Spampinato <spampinabio@gmail.com>
2021-05-19 15:14:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Infinity Set
+
+A `Set` that doesn't throw if you put more than 16 million items in it (16777216 to be exact).
+
+Because that's what the native `Set` object does for some reason.
+
+## Install
+
+```sh
+npm install --save infinity-set
+```
+
+## Usage
+
+It works just like `Set`, but it doesn't make you cry.
+
+## License
+
+MIT © Fabio Spampinato
---
+++ src/index.ts
@@ -0,0 +1,125 @@
+
+/* INFINITY SET */
+
+class InfinitySet<T> {
+
+  /* VARIABLES */
+
+  private current: Set<T>;
+  private pool: Set<T>[];
+
+  /* CONSTRUCTOR */
+
+  constructor () {
+
+    this.clear ();
+
+  }
+
+  /* GETTERS */
+
+  get size (): number {
+
+    return this.pool.reduce ( ( sum, set ) => sum + set.size, 0 );
+
+  }
+
+  /* API */
+
+  add ( value: T ): this {
+
+    if ( this.has ( value ) ) return this;
+
+    this.current.add ( value );
+
+    if ( this.current.size === 16777215 ) {
+
+      this.current = new Set<T> ();
+      this.pool.push ( this.current );
+
+    }
+
+    return this;
+
+  }
+
+  clear (): undefined {
+
+    this.current = new Set<T> ();
+    this.pool = [this.current];
+
+    return;
+
+  }
+
+  has ( value: T ): boolean {
+
+    return this.pool.some ( set => set.has ( value ) );
+
+  }
+
+  delete ( value: T ): boolean {
+
+    return this.pool.some ( set => set.delete ( value ) );
+
+  }
+
+  /* ITERATION API */
+
+  * [Symbol.iterator] (): IterableIterator<T> {
+
+    for ( const set of this.pool ) {
+
+      yield * set[Symbol.iterator]();
+
+    }
+
+  }
+
+  * keys (): IterableIterator<T> {
+
+    for ( const set of this.pool ) {
+
+      yield * set.keys ();
+
+    }
+
+  }
+
+  * values (): IterableIterator<T> {
+
+    for ( const set of this.pool ) {
+
+      yield * set.values ();
+
+    }
+
+  }
+
+  * entries (): IterableIterator<[T, T]> {
+
+    for ( const set of this.pool ) {
+
+      yield * set.entries ();
+
+    }
+
+  }
+
+  forEach ( callback: ( value: T, key: T, set: InfinitySet<T> ) => void, thisArg?: any ): undefined {
+
+    for ( const value of this ) {
+
+      callback.call ( thisArg, value, value, this );
+
+    }
+
+    return;
+
+  }
+
+};
+
+/* EXPORT */
+
+export default InfinitySet;
